ed516dae25988680a3f12c14de501cf1fa44b35a
kevin sinclair <kevin.sinclair@engineering.digital.dwp.gov.uk>
2025-01-20 19:02:15 +0000

    modified routing etc

--- app/routes/agent-ui-rep.js
+++ app/routes/agent-ui-rep.js
@@ -11,6 +11,18 @@ router.post('/v22/agent-ui-rep/search', function (req, res) {
     const searchNumber = req.session.data['search-number']
     // console.log("search number is: " + searchNumber);
     if (searchNumber == 'AB112233C') {
+        test = req.session.data['test-var']
+        if (!test){
+            req.session.data['claimant-ni'] = 'AB112233C'
+            req.session.data['claimant-first-name'] = 'Janet'
+            req.session.data['claimant-last-name'] = 'Harding'
+            req.session.data['claimant-email'] = 'jh@gmail.com'
+            req.session.data['claimant-dob'] = '12 January 1946'
+            req.session.data['claimant-postcode'] = 'SW9 6GR'
+            req.session.data['claimant-mobile'] = '07876543210'
+            req.session.data['test-var'] = 'true'
+        }
+        
         res.redirect('/v22/agent-ui-rep/details-record-b')
     } else {
         res.redirect('/v22/agent-ui-rep/info')
